ca04d272e8b29082ea1c09a1b085cd3e540a5d4c
mazharul99tech <mazharul99tech@gmail.com>
2020-09-05 22:22:14 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2)
+}
+
+const premik = {name: "Mazharul", phone: 345432};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+// null
+
+let ages = [3, 43, 64];
+console.log(ages[11]);

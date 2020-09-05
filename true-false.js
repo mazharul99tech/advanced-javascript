8ac2774318612249d35af83e74b5c774b479c6f7
mazharul99tech <mazharul99tech@gmail.com>
2020-09-05 21:12:55 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+//falsy:
+//false
+//0
+//NaN
+//null
+//undefined
+// ""
+
+//Truty:
+//"0", " ", [], {},
+
+
+// const name = "0";
+
+let name = 12;
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

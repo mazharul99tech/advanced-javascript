9c8d548d37dbf42ef835dfe99e2e225c2830011f
mazharul99tech <mazharul99tech@gmail.com>
2020-09-06 16:20:07 +0600

    closure added and more functinality

---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+
+    if (result > 9) {
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funkcy";
+        mood = "cranky";
+        // console.log(mood);
+    }
+    
+    let day = "Black Friday";
+    console.log(day);
+    return result;
+}
+
+const output = sum(3, 7);
+//console.log(bonus);
+//console.log(output);

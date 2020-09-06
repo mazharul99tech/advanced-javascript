942d0c7f63103366594685068c792d01f5aaef55
mazharul99tech <mazharul99tech@gmail.com>
2020-09-06 17:06:21 +0600

    array split done

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums =[1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2, 5);
+
+const removed = nums.splice(2, 3, 34);
+console.log(removed);
+
+
+const together = nums.join("ami ");
+console.log(together);
